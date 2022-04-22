456522e8b0e8f71ea0351110559b8066830a2ac4
Ignacio Chitnisky <ignaciochitnisky@192.168.1.7>
2022-04-21 23:48:58 -0300

    trying

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,17 +5,17 @@ def canUnlockAll(boxes):
     '''
     if (len(boxes) == 0):
         return False
-    eachbox = list(range(len(boxes)))
+    box = list(range(len(boxes)))
     key = [False]*len(boxes)
-    eachkeys = [0]
+    keys = [0]
     key[0] = True
 
-    for i in eachkeys:
-        for tk in boxes[i]:
-            if tk in eachbox:
-                if tk not in eachkeys:
-                    eachkeys.append(tk)
-                    key[tk] = True
+    for i in keys:
+        for k in boxes[i]:
+            if k in box:
+                if k not in keys:
+                    keys.append(k)
+                    key[k] = True
 
     if False in key:
         return False
