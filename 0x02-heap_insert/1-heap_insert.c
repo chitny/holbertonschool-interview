5aecfd0686269badb308c6436cc4f8739f0e5fd1
chitny <75819353+chitny@users.noreply.github.com>
2022-05-05 21:44:33 -0300

    Update 1-heap_insert.c

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -7,16 +7,16 @@
 **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t binary_tree_size = 0;
+	size_t bt_size = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL)
-		binary_tree_size = binary_tree_size + binary_tree_size(tree->left);
+		bt_size = bt_size + binary_tree_size(tree->left);
 	if (tree->right != NULL)
-		binary_tree_size = binary_tree_size + binary_tree_size(tree->right);
-	binary_tree_size = binary_tree_size + 1;
-	return (binary_tree_size);
+		bt_size = bt_size + binary_tree_size(tree->right);
+	bt_size = bt_size + 1;
+	return (bt_size);
 }
 /**
  * binary_tree_is_perfect - checks if a tree is perfect
